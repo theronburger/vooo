1a5ee3a0381961ff38fb08e2080fcd9b4c435f7e
theronburger <dev@theronburger.com>
2023-02-06 15:34:54 +0100

    tsx delete

--- tilesets/test01.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="test01" tilewidth="32" tileheight="32" tilecount="451" columns="41">
- <image source="test01.png" width="1336" height="375"/>
-</tileset>
